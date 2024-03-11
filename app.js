9b31f4edde6eba478ce025e7913bfef5976dfcc8
arsequena <alexandersequena@gmail.com>
2024-03-11 20:45:51 +0800

    add saving of username

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 angular.module('chatApp', [])
     .controller('ChatController', function($window, $scope) {
         var chat = this;
+        chat.username = null;
+        chat.newUsername = '';
+
+        chat.setUsername = function() {
+            if (chat.newUsername) {
+                chat.username = chat.newUsername;
+            }
+        };
         
+
     });
