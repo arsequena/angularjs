af407596f0f1b4f003f5f355ac1a3dbf3cf190bc
arsequena <alexandersequena@gmail.com>
2024-03-11 20:50:10 +0800

    add saving of messages

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ angular.module('chatApp', [])
         var chat = this;
         chat.username = null;
         chat.newUsername = '';
+        chat.newMessage = '';
+        chat.messages = [];
 
         chat.setUsername = function() {
             if (chat.newUsername) {
                 chat.username = chat.newUsername;
             }
         };
-        
+       
+        chat.sendMessage = function() {
+            if (chat.newMessage) {
+                chat.messages.push({ ts:Date.now(), username: chat.username, text: chat.newMessage });
+                chat.newMessage = '';
+            }
+        };
+
 
     });
